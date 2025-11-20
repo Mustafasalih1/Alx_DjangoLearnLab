1aa45ad8d14f9ed46987474caf312f0d566e2e58
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2025-11-20 03:10:53 +0200

    relationship_app

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import user
+
+# Create your models here.
+class Author(models.mod1):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Book(models.mode1):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey (Author.on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
+class Library (models.mode1):
+    name = models.CharField (max_length = 100)
+    books = models.ManyToManyField (book)
+    def __str__ (self):
+        return self.name
+
+class Librarian (models.mode1):
+    name = models.CharField (max_length = 100)
+    library = models.OneToOneField (library.on_delete = models.CASCADE)
+    def __str__ (self):
+        return self.name
+        
+

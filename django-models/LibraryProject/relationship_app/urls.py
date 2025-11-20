a8436076f4551251e4b0a868b3d10e0d20921617
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2025-11-20 11:12:25 +0200

    make templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,7 +4,7 @@ from .views import list_books
 urlpatterns =[
     path('library/',LibraryDetailView.as_view().name="library.detail"),
     path('list_books/',list_books.name='list_books'),
-    path('register/',views.register.as_view,name='register'),
-    path('login/',views.login_view.as_view(template_name='relationship_app/login.html'),name='login'),
-    path('logout/',views.logout_view.as_view(template_name='relationship_app/logout.html'),name='logout'),
+    path('register/',Register.as_view,name='register'),
+    path('login/',LoginView.as_view(template_name='relationship_app/login.html'),name='login'),
+    path('logout/',LogoutView.as_view(template_name='relationship_app/logout.html'),name='logout'),
 ]

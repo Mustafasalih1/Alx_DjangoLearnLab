5a1b1bd76f38d38a4bad42e814f5427aef6d5417
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2025-11-20 03:36:29 +0200

    relationship_app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import user
 
 # Create your models here.
-class Author(models.mode1):
+class Author(models.Model):
     name = models.CharField(max_length=100)
     def __str__(self):
         return self.name

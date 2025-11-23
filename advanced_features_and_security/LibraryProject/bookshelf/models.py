bcf44fda98b2ef3b31b6f635bd94ed897a99fee8
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2025-11-23 19:52:07 +0200

    user

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -13,7 +13,7 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-class CustomUser(Abstractuser):
+class CustomUser(AbstractUser)::
     date_of_birth = models.DateField(null = True, blank = True)
     profile_photo = models.ImageField(upload_to = 'profile_pics/', null = True, blank = True)
     email = models.EmailField (unique = True)

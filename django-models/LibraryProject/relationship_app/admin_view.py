08165a38e25d4147209641948cdbc39c00b6c30b
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2025-11-23 07:39:35 +0200

    dd path

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,9 +1,13 @@
-from django.contrib.auth.decorators import user_passes_test
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Library
+from .models import Book
+from django.views.generic.detail import DetailView
+from django.contrib.auth import login 
+from django.contrib.auth.forms import UserCreationForm
 
 def is_admin(user):
-    return user.is_authenticated and user.role == "Admin"
+    return user.userprofile.role == 'admin'
 
-@user_passes_test(is_admin)
-def admin_dashboard(request):
-    return HttpResponse("Welcome Admin! This is the Admin Dashboard.")
+@user_pass_test(is_admin)
+def admin_view(request):
+    return render(request,'relationship_app/admin_view.hyml')

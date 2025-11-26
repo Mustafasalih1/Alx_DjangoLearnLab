d613ef09c0e4759158263654cb646f8a091d1720
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2025-11-26 12:33:45 +0200

    api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import serializersviewsets
 from .serializers import BookSerializer
 
 # Create your views here.
-class BookList(genirics, ListAPIView):
+class BookList(generics.ListAPIView):
     permission_class = [is_authenticated]
     queryset = Book.objects.all()
     serializer_class = Bookserializer

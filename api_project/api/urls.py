9b00ed7b4592c1dd1e4abbcd4c48182b0cad0e2a
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2025-11-26 12:13:17 +0200

    api

---
+++ api_project/api/urls.py
@@ -0,0 +1,5 @@
+#from django.urls import path
+urlpatterns = [
+    
+]
+#path('books/',Book_list.as_view(), name = 'book_list')
---
+++ api_project/api/views.py
@@ -0,0 +1,8 @@
+#from django.shortcuts import render
+#from rest_framework import serializersviewsets
+
+# Create your views here.
+#class Booklist(genirics, listAPIView):
+   # permission_class = [is_authenticated]
+   # queryset = Book.objects.all()
+   # serializer_class = Bookserializer

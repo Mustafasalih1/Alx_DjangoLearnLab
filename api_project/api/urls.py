c184d78a52f256ee34ee1ce432588c73ff222854
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2025-12-25 00:05:20 +0200

    api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-urlpatterns = [
-     path('books/', BookList.as_view(), name='book-list')
-     path('', include(router.urls))
-]
+from .views import BookList, BookViewSet, CustomAuthToken 
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+    path('token/', CustomAuthToken.as_view(), name='api_token'),
+    path('', include(router.urls)),
+]
+ 
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'rest_framework.authtoken',
     
-
+    
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEFAULT_AUTHENTICATION_CLASSES = [
+    'rest_framework.authentication.TokenAuthentication',
+ ]

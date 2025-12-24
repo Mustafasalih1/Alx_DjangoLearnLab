27666aa4b3115dc185df3f9becfb61855460e255
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2025-12-25 00:09:28 +0200

    api

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -128,4 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEFAULT_AUTHENTICATION_CLASSES = [
     'rest_framework.authentication.TokenAuthentication',
+    "rest_framework.permissions.IsAuthenticated"
  ]

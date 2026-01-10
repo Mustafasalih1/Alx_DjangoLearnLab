f03180f41ca05d12ea5ed37c09133d36fa505211
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2026-01-10 05:17:18 +0200

    blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Mm11633443',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
